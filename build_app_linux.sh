5201bb09bab30251b271febd8c7bfc1006c0e99e
Gerrit Grunwald <han.solo@mac.com>
2022-02-28 11:32:11 +0100

    Adjusted build files to 17.0.15

--- build_app_linux.sh
+++ build_app_linux.sh
@@ -19,8 +19,8 @@
 #
 
 JAVA_VERSION=17
-MAIN_JAR="GlucoStatusFX-17.0.14.jar"
-APP_VERSION=17.0.14
+MAIN_JAR="GlucoStatusFX-17.0.15.jar"
+APP_VERSION=17.0.15
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
