114c31e12e9ba581f9c805675c504296bff97edc
Gerrit Grunwald <han.solo@mac.com>
2022-02-28 10:05:00 +0100

    cosmetics

--- src/main/java/eu/hansolo/fx/glucostatus/Records.java
+++ src/main/java/eu/hansolo/fx/glucostatus/Records.java
@@ -18,10 +18,7 @@
 
 package eu.hansolo.fx.glucostatus;
 
-import java.time.Instant;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Objects;
 
 
