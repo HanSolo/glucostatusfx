9f8f60f3dde909cb7de132568ef78b684e4aa1bd
Gerrit Grunwald <han.solo@mac.com>
2022-03-03 08:02:46 +0100

    cosmetics

--- src/main/java/eu/hansolo/fx/glucostatus/Main.java
+++ src/main/java/eu/hansolo/fx/glucostatus/Main.java
@@ -1203,7 +1203,6 @@ public class Main extends Application {
         int           startHour     = startTime.getHour();
         int           endHour       = endTime.getHour();
         boolean       startsAtNight = false;
-        boolean       endsAtNight   = false;
         List<eu.hansolo.toolboxfx.geom.Rectangle> nights = new ArrayList<>();
 
         // Chart starts at night
@@ -1240,8 +1239,7 @@ public class Main extends Application {
             lastHour = h;
         }
 
-        if (nightStart) { // && (endTime.getMonthValue() > startTime.getMonthValue() ||
-                           //endTime.getDayOfMonth() > startTime.getDayOfMonth())) {
+        if (nightStart) {
             nights.add(new eu.hansolo.toolboxfx.geom.Rectangle(nightX, GRAPH_INSETS.getTop(), availableWidth - nightX + GRAPH_INSETS.getRight(), availableHeight));
         }
 
