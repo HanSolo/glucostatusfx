07f19b8d74f8e67c874a582d224b91754a533651
Gerrit Grunwald <han.solo@mac.com>
2022-03-07 13:21:05 +0100

    Fixed issue in 30 days view and adjusted colors for iOS

--- src/main/java/eu/hansolo/fx/glucostatus/Main.java
+++ src/main/java/eu/hansolo/fx/glucostatus/Main.java
@@ -922,7 +922,7 @@ public class Main extends Application {
         boolean darkMode        = true;
 
         ctx.clearRect(0, 0, width, height);
-        ctx.setFill(darkMode ? Color.rgb(30, 28, 26) : Color.rgb(234, 233, 233));
+        ctx.setFill(darkMode ? Constants.DARK_BACKGROUND : Constants.BRIGHT_BACKGROUND);
         ctx.fillRect(0, 0, width, height);
         ctx.setFont(ticklabelFont);
         ctx.setFill(Constants.BRIGHT_TEXT);
