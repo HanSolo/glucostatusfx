05b1acbf39f6233842a192457e0b92af5de7a475
Gerrit Grunwald <han.solo@mac.com>
2023-07-14 16:40:51 +0200

    Adjusted build scripts

--- build_app_macos.sh
+++ build_app_macos.sh
@@ -19,8 +19,8 @@
 #
 
 JAVA_VERSION=17
-MAIN_JAR="GlucoStatusFX-17.0.31.jar"
-APP_VERSION=17.0.31
+MAIN_JAR="GlucoStatusFX-17.0.33.jar"
+APP_VERSION=17.0.33
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 set JAVA_VERSION=17
-set MAIN_JAR=GlucoStatusFX-17.0.31.jar
-set APP_VERSION=17.0.31
+set MAIN_JAR=GlucoStatusFX-17.0.33.jar
+set APP_VERSION=17.0.33
 
 rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
 rem Remove previously generated java runtime and installers. Copy all required
