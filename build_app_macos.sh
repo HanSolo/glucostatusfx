f28f9ee9e0b7fe60eb772269f2bdbcba5bc2b37c
Gerrit Grunwald <han.solo@mac.com>
2025-07-29 12:58:18 +0200

    Moved to 23.0.3

--- build_app_macos.sh
+++ build_app_macos.sh
@@ -19,8 +19,8 @@
 #
 
 JAVA_VERSION=23
-MAIN_JAR="GlucoStatusFX-23.0.1.jar"
-APP_VERSION=23.0.1
+MAIN_JAR="GlucoStatusFX-23.0.3.jar"
+APP_VERSION=23.0.3
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
