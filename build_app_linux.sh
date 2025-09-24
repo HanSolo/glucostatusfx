af52c592fa3153ce427896d68aa2ce130abce574
Gerrit Grunwald <han.solo@mac.com>
2025-09-24 18:41:24 +0200

    First build on JDK 25

--- build_app_linux.sh
+++ build_app_linux.sh
@@ -18,9 +18,9 @@
 # limitations under the License.
 #
 
-JAVA_VERSION=23
-MAIN_JAR="GlucoStatusFX-23.0.3.jar"
-APP_VERSION=23.0.3
+JAVA_VERSION=25
+MAIN_JAR="GlucoStatusFX-25.0.0.jar"
+APP_VERSION=25.0.0
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
